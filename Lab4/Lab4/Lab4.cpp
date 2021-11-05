f74f2a575fec56f91cd125dcdf12e8ac6b264b29
Sasha Videvich <fpm.videvich@bsu.by>
2021-11-05 09:15:06 +0300

    Perepisal labu dlya Pashi =)

--- Lab4/Lab4/Lab4.cpp
+++ Lab4/Lab4/Lab4.cpp
@@ -5,51 +5,57 @@
 
 using namespace std;
 
+const char g_endString('\0');
+
 int my_atoi(const char* inputString) {
 	int result = 0;
-	for (int i = 0; inputString[i] != '\0'; i++) {
-		if (inputString[i] > '0' && inputString[i] < '9')
-			result = result * 10 + (inputString[i] - '0');
+    char zero = '0';
+    char nine =  '9';
+    char minus = '-';
+	for (int i = 0; inputString[i] != g_endString; i++) {
+		if (inputString[i] > zero && inputString[i] < nine)
+			result = result * 10 + (inputString[i] - zero);
 		else
-			if (i == 0 && inputString[i] == '-')
+			if (i == 0 && inputString[i] == minus)
 				result = result;
 			else
 				break;
 	}
-	if (inputString[0] == '-')
+	if (inputString[0] == minus)
 		result = -result;
 	return result;
 }
 
-int find_the_most_frequent_element(int firstSpace, int lastSpace, char* inputString) {
-	vector<char> all_letters;
-	vector<int> number_of_the_same_letters(26);
+int find_the_most_frequent_element(int firstSpace, int lastSpace, const char* inputString) {
+	int numberOfLettersInAlphabet = 26;
+    vector<char> all_letters;
+	vector<int> number_of_the_same_letters(numberOfLettersInAlphabet);
 	int number_of_letters = 'a';
-	for (int i = 0; i < 26; i++) {
+	for (int i = 0; i < numberOfLettersInAlphabet; i++) {
 		all_letters.push_back(number_of_letters);
 		number_of_letters++;
 	}
-	for (int i = 0; i < number_of_the_same_letters.size(); i++)
-		number_of_the_same_letters[i] = 0;
+	for (int & number_of_the_same_letter : number_of_the_same_letters)
+		number_of_the_same_letter = 0;
 	for (int i = firstSpace; i < lastSpace; i++) {
-		for (int j = 0; j < 26; j++) {
+		for (int j = 0; j < numberOfLettersInAlphabet; j++) {
 			if (inputString[i] == all_letters[j])
 				number_of_the_same_letters[j]++;
 		}
 	}
 	int max_result = INT_MIN;
-	for (int i = 0; i < number_of_the_same_letters.size(); i++) {
-		if (number_of_the_same_letters[i] > max_result) {
-			max_result = number_of_the_same_letters[i];
+	for (int number_of_the_same_letter : number_of_the_same_letters) {
+		if (number_of_the_same_letter > max_result) {
+			max_result = number_of_the_same_letter;
 		}
 	}
 	return max_result;
 }
 
-int CountLetters(char* arary_for_string_TASK2, char input) {
+int CountLetters(const char* array_for_string_TASK2, char input) {
 	int number_of_letters = 0;
-	for (int i = 0; arary_for_string_TASK2[i] != input; i++) {
-		if (arary_for_string_TASK2[i] == '.') {
+	for (int i = 0; array_for_string_TASK2[i] != input; i++) {
+		if (array_for_string_TASK2[i] == '.') {
 			break;
 		}
 		else {
@@ -59,21 +65,23 @@ int CountLetters(char* arary_for_string_TASK2, char input) {
 	return number_of_letters;
 }
 
-void find_words(char input,char* arary_for_string_TASK2, vector<int>& first_space, vector<int>& last_space) {
+void find_words(char input,char* array_for_string_TASK2, vector<int>& first_space, vector<int>& last_space) {
 	first_space.push_back(0);
-	for (int i = 0; arary_for_string_TASK2[i] != input; i++) {
-		if (arary_for_string_TASK2[i] == ' ' || arary_for_string_TASK2[i] == '.') {
+	for (int i = 0; array_for_string_TASK2[i] != input; i++) {
+		if (array_for_string_TASK2[i] == ' ' || array_for_string_TASK2[i] == '.') {
 			first_space.push_back(i);
 			last_space.push_back(i);
 		}
 	}
-	last_space.push_back(CountLetters(arary_for_string_TASK2, input));
+	last_space.push_back(CountLetters(array_for_string_TASK2, input));
 }
 
-void result(char* arary_for_string_TASK2, vector<int>& first_space, vector<int>& last_space) {
+void result(char* array_for_string_TASK2, vector<int>& first_space, vector<int>& last_space) {
 	vector<int> result_vector;
-	for (int i = 0; i < last_space.size(); i++)
-		result_vector.push_back(find_the_most_frequent_element(first_space[i], last_space[i], arary_for_string_TASK2));
+    result_vector.reserve(last_space.size());
+for (int i = 0; i < last_space.size(); i++) {
+        result_vector.push_back(find_the_most_frequent_element(first_space[i], last_space[i], array_for_string_TASK2));
+    }
 	int result = INT_MIN, for_i = 0;
 	for (int i = 0; i < result_vector.size(); i++) {
 		if (result_vector[i] > result) {
@@ -85,32 +93,32 @@ void result(char* arary_for_string_TASK2, vector<int>& first_space, vector<int>&
 	for_the_first_spaces = first_space[for_i];
 	for_the_last_spaces = last_space[for_i];
 	for (int i = for_the_first_spaces; i < for_the_last_spaces; i++)
-		cout << arary_for_string_TASK2[i];
+		cout << array_for_string_TASK2[i];
 
 }
 
 int main() {
-	cout << "Enter the size of your stirng" << endl;
+	cout << "Enter the size of your string" << endl;
 	int size;
 	cin >> size;
-	char* arary_for_string_TASK1 = new char[size + 1];
-	arary_for_string_TASK1[size] = '\0';
+	char* array_for_string_TASK1 = new char[size + 1];
+	array_for_string_TASK1[size] = g_endString;
 	cout << "Enter the elements of your string" << endl;
 	for (int i = 0; i < size; i++)
-		cin >> arary_for_string_TASK1[i];
-	cout << "TASK 1" << endl << my_atoi(arary_for_string_TASK1) << endl;
+		cin >> array_for_string_TASK1[i];
+	cout << "TASK 1" << endl << my_atoi(array_for_string_TASK1) << endl;
 	const int max_size = 300;
 	vector<int>first_space;
 	vector<int>last_space;
-	char* arary_for_string_TASK2 = new char[max_size];
+	char* array_for_string_TASK2 = new char[max_size];
 	cout << "Enter some text" << endl;
 	char input = NULL;
 	for (int i = 0; input != '.'; i++) {
 		cin.get(input);
-		arary_for_string_TASK2[i] = input;
+		array_for_string_TASK2[i] = input;
 	}
-	find_words(input, arary_for_string_TASK2, first_space, last_space);
-	result(arary_for_string_TASK2, first_space, last_space);
+	find_words(input, array_for_string_TASK2, first_space, last_space);
+	result(array_for_string_TASK2, first_space, last_space);
 	run_tests();
 	return 0;
 }
--- Lab4/Lab4/functions.h
+++ Lab4/Lab4/functions.h
@@ -1,5 +1,5 @@
 #pragma once
 
 int my_atoi(const char* inputString);
-int find_the_most_frequent_element(int firstSpace, int lastSpace, char* inputString);
-int CountLetters(char* arary_for_string_TASK2, char input);
+int find_the_most_frequent_element(int firstSpace, int lastSpace, const char* inputString);
+int CountLetters(const char* array_for_string_TASK2, char input);
